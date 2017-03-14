0b3b9abc13b367f44d6aa2d1979d22299961785f
Victor Vucicevich (Vic Vu) <victorvuc@gmail.com>
2017-03-14 16:08:58 -0400

    Update TRNBroadcastReceiver.java

--- android/src/main/java/com/taplytics/react/TRNBroadcastReceiver.java
+++ android/src/main/java/com/taplytics/react/TRNBroadcastReceiver.java
@@ -8,33 +8,41 @@ import com.facebook.react.bridge.WritableMap;
 import com.taplytics.sdk.TLGcmBroadcastReceiver;
 
 /**
- * Created by Adam on 2017-01-30.
+ * Created by Adam Wootton on 2017-01-30.
  */
-
 public class TRNBroadcastReceiver extends TLGcmBroadcastReceiver {
     @Override
     public void pushOpened(Context context, Intent intent) {
         WritableMap data = Arguments.createMap();
-        data.putString("value", intent.getExtras().getString("custom_keys"));
+        if (intent.getExtras() != null) {
+            data.putString("value", intent.getExtras().getString("custom_keys"));
+        }
         if (TaplyticsReactModule.getInstance() != null) {
             TaplyticsReactModule.getInstance().sendEvent("pushOpened", data);
         }
-        super.pushOpened(context, intent);
+        super.pushOpened(context,intent);
     }
+
     @Override
     public void pushDismissed(Context context, Intent intent) {
         WritableMap data = Arguments.createMap();
-        data.putString("value", intent.getExtras().getString("custom_keys"));
+        if (intent.getExtras() != null) {
+            data.putString("value", intent.getExtras().getString("custom_keys"));
+        }
         if (TaplyticsReactModule.getInstance() != null) {
             TaplyticsReactModule.getInstance().sendEvent("pushDismissed", data);
         }
     }
+
     @Override
     public void pushReceived(Context context, Intent intent) {
         WritableMap data = Arguments.createMap();
-        data.putString("value", intent.getExtras().getString("custom_keys"));
+        if (intent.getExtras() != null) {
+            data.putString("value", intent.getExtras().getString("custom_keys"));
+        }
         if (TaplyticsReactModule.getInstance() != null) {
             TaplyticsReactModule.getInstance().sendEvent("pushReceived", data);
         }
     }
 }
+
