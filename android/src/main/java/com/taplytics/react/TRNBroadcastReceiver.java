f4e59edd31c5cc510866297677a7e669de84dfda
vicv <victorvuc@gmail.com>
2017-03-21 12:28:02 -0400

    Return correct data object in android push

--- android/src/main/java/com/taplytics/react/TRNBroadcastReceiver.java
+++ android/src/main/java/com/taplytics/react/TRNBroadcastReceiver.java
@@ -7,42 +7,49 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
 import com.taplytics.sdk.TLGcmBroadcastReceiver;
 
+import org.json.JSONObject;
+
 /**
  * Created by Adam Wootton on 2017-01-30.
  */
+
 public class TRNBroadcastReceiver extends TLGcmBroadcastReceiver {
     @Override
     public void pushOpened(Context context, Intent intent) {
-        WritableMap data = Arguments.createMap();
-        if (intent.getExtras() != null) {
-            data.putString("value", intent.getExtras().getString("custom_keys"));
-        }
         if (TaplyticsReactModule.getInstance() != null) {
-            TaplyticsReactModule.getInstance().sendEvent("pushOpened", data);
+            TaplyticsReactModule.getInstance().sendEvent("pushOpened", getIntentData(intent));
         }
-        super.pushOpened(context,intent);
+        super.pushOpened(context, intent);
     }
 
     @Override
     public void pushDismissed(Context context, Intent intent) {
-        WritableMap data = Arguments.createMap();
-        if (intent.getExtras() != null) {
-            data.putString("value", intent.getExtras().getString("custom_keys"));
-        }
         if (TaplyticsReactModule.getInstance() != null) {
-            TaplyticsReactModule.getInstance().sendEvent("pushDismissed", data);
+            TaplyticsReactModule.getInstance().sendEvent("pushDismissed", getIntentData(intent));
         }
     }
 
     @Override
     public void pushReceived(Context context, Intent intent) {
-        WritableMap data = Arguments.createMap();
-        if (intent.getExtras() != null) {
-            data.putString("value", intent.getExtras().getString("custom_keys"));
-        }
         if (TaplyticsReactModule.getInstance() != null) {
-            TaplyticsReactModule.getInstance().sendEvent("pushReceived", data);
+            TaplyticsReactModule.getInstance().sendEvent("pushReceived", getIntentData(intent));
         }
     }
-}
 
+    private WritableMap getIntentData(Intent intent) {
+        WritableMap data = Arguments.createMap();
+        JSONObject dataObject = new JSONObject();
+        try {
+            if (intent.hasExtra("tl_id")) {
+                dataObject.put("tl_id", intent.getExtras().getString("tl_id"));
+            }
+            if (intent.hasExtra("custom_keys")) {
+                dataObject.put("custom_keys", new JSONObject(intent.getExtras().getString("custom_keys")));
+            }
+        } catch (Throwable ignored) {
+            //Ignore the bad JSON for now. Log here as you please.
+        }
+        data.putString("value", dataObject.toString());
+        return data;
+    }
+}
